843c903d19626561c903f8f26669a7efa617e25a
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-29 20:03:56 +0600

    setup root layout and constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { ArrowUpIcon } from "lucide-react";
-
+export const metadata = {
+  title: 'Home'
+}
 
 export default function page() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
-      <Button variant="outline">Button</Button>
-      <h1>Hello</h1>
-      <Button variant="outline" size="icon" aria-label="Submit">
-        <ArrowUpIcon />
-      </Button>
+      Home
     </div>
   )
 }
