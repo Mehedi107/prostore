71ef20ff0b90bb365ea1b2d79c7dc9c288b17a89
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-29 16:37:38 +0600

    setup shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { ArrowUpIcon } from "lucide-react";
+
+
 export default function page() {
-  return <>prostore</>
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <ArrowUpIcon />
+      </Button>
+    </div>
+  )
 }
