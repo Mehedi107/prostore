5b4b35394db948b690a3a990bd47e4685657d579
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-29 19:22:09 +0600

    change font and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function page() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
       <Button variant="outline">Button</Button>
+      <h1>Hello</h1>
       <Button variant="outline" size="icon" aria-label="Submit">
         <ArrowUpIcon />
       </Button>
